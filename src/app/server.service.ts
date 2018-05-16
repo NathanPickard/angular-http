04f6566e2bd01047ec35c89733a91000fa0e31e0
Nathan Pickard <nathan.pickard87@gmail.com>
2018-05-15 18:12:19 -0700

    Set up GET request

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 
 @Injectable()
 export class ServerService {
   constructor(private http: Http) { }
 
   storeServers(servers: any[]) {
-    return this.http.post('https://angular-http-8778a.firebaseio.com/data.json', servers);
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    return this.http.post('https://angular-http-8778a.firebaseio.com/data.json', servers, { headers: headers });
+  }
+
+  getServers() {
+    return this.http.get('https://angular-http-8778a.firebaseio.com/data.json');
   }
 }
