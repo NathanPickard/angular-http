0f10c84e1fa3ad9972dc969d6668672868877d4b
Nathan Pickard <nathan.pickard87@gmail.com>
2018-05-15 21:20:55 -0700

    Transforming Responses with observable opters (map) and catching errors

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http';
+import { Http, Headers, Response } from '@angular/http';
+import 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ServerService {
@@ -7,10 +9,25 @@ export class ServerService {
 
   storeServers(servers: any[]) {
     const headers = new Headers({ 'Content-Type': 'application/json' });
-    return this.http.post('https://angular-http-8778a.firebaseio.com/data.json', servers, { headers: headers });
+    // return this.http.post('https://angular-http-8778a.firebaseio.com/data.json', servers, { headers: headers });
+    return this.http.put('https://angular-http-8778a.firebaseio.com/data.json', servers, { headers: headers });
   }
 
   getServers() {
-    return this.http.get('https://angular-http-8778a.firebaseio.com/data.json');
+    return this.http.get('https://angular-http-8778a.firebaseio.com/data')
+      .map(
+        (response: Response) => {
+          const data = response.json();
+          for (const server of data) {
+            server.name = 'FETCHED_' + server.name;
+          }
+          return data;
+        }
+      )
+      .catch(
+        (error: Response) => {
+          return Observable.throw('Something went wrong');
+        }
+      );
   }
 }
